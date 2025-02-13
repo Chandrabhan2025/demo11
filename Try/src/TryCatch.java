29598291d4e44fe1df177d2105062d9d3153a7ac
Chandrabhan2025 <chandrabhan.s.2002@gmail.com>
2025-02-13 16:54:38 +0530

    first commit

---
+++ Try/src/TryCatch.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TryCatch {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		try( InputStreamReader io = new InputStreamReader(null);
+				BufferedReader bufferedReader = new BufferedReader(io)) {	
+		} 
+
+	}
+
+}
