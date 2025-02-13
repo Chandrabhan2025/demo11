dfd1cf9e28818359ffa827a0a45a006078e4ef21
Chandrabhan2025 <chandrabhan.s.2002@gmail.com>
2025-02-13 17:19:20 +0530

    second commit

---
+++ user/src/main/java/com/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/*Author Name : Darshan Bambal 
+*Date: 22-11-2024
+*Descriptions : This class is created to create the entity 'user' in 'insurance' database 
+ */
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+
+    private String email;
+    private String password;
+    private String role;
+}
